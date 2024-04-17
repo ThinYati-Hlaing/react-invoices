30fa5bd01065234c27913b37102a708f8a0d738b
thinyati-hlaing <thinyatihlaing6@gmail.com>
2024-04-17 15:35:25 +0630

    update

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -10,8 +10,7 @@ const Drawer = () => {
     <div id="productDrawer" className={`h-screen w-96 bg-white fixed right-0 shadow-lg overflow-scroll duration-200 ${openDrawer && "translate-x-full"}`}>
       <div className="p-3 flex items-center justify-between">
         <div >
-          <h1 className="text-3xl font-bold text-gray-700">Your Product</h1>
-          <h4 className="text-xl text-gray-500">Manage Product</h4>
+          <h1 className="text-3xl font-bold text-gray-700">Manage Product</h1>
         </div>
         <button onClick={toggleDrawer} id="closeDrawer" className="p-3 bg-blue-100 text-blue-600 active:scale-90 duration-200">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
